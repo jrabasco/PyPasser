17fe463a2313d565bfbd94fba5087a8cabe53183
Jeremy Rabasco <jeremy.rabasco@epfl.ch>
2015-02-02 16:16:02 +0100

    Added test script

--- run_tests.py
+++ run_tests.py
@@ -1,1 +1,16 @@
-__author__ = 'daroud'
+#!/usr/bin/python3.4
+
+__author__ = 'Jeremy Rabasco'
+
+import os
+
+python_executable = "python3.4"
+
+test_script_name = os.path.basename(__file__)
+
+
+for (dirpath, dirnames, filenames) in os.walk(os.getcwd()):
+    for file in filenames:
+        if file.endswith(".py") and file != test_script_name:
+            print("Testing", file)
+            os.system(python_executable + " " + file)

f2899b24e43b14bdd7d9a2aefa8ab9a87c10f953
Jeremy Rabasco <jeremy.rabasco@epfl.ch>
2015-02-02 17:04:46 +0100

    Added serialization to the service and renamed file

---
+++ service.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3.4
+
+__author__ = 'Jeremy Rabasco'
+
+import pickle
+
+class Service:
+
+    def __init__(self, service_name: str="ServiceName", username: str="Username", password: str="Password"):
+        self.__service_name = service_name
+        self.__username = username
+        self.__password = password
+
+    @property
+    def service_name(self) -> str:
+        return self.__service_name
+
+    @service_name.setter
+    def service_name(self, service_name: str):
+        self.__service_name = service_name
+
+    @property
+    def username(self) -> str:
+        return self.__username
+
+    @username.setter
+    def username(self, username: str):
+        self.__username = username
+
+    @property
+    def password(self) -> str:
+        return self.__password
+
+    @password.setter
+    def password(self, password: str):
+        self.__password = password
+
+    def write(self, file: str=""):
+        if file == "":
+            file = self.service_name
+        with open(file, "wb+") as out_file:
+            pickle.dump(self, out_file)
+
+    def read(self, file: str) :
+        with open(file, "rb") as in_file:
+            tmp = pickle.load(in_file)
+            self.service_name = tmp.service_name
+            self.username = tmp.username
+            self.password = tmp.password
+
+if __name__ == "__main__":
+    import os
+    ref_service = Service()
+    print("Creation of service with no argument :", end="")
+    service1 = Service()
+    assert service1.service_name == ref_service.service_name
+    assert service1.username == ref_service.username
+    assert service1.password == ref_service.password
+    print(" OK")
+
+    print("Creation of service with arguments :", end="")
+    service1 = Service("service", "user", "pass")
+    assert service1.service_name == "service"
+    assert service1.username == "user"
+    assert service1.password == "pass"
+    print(" OK")
+
+    print("Modifying service attributes :", end="")
+    service1 = Service()
+    service1.service_name = "ser"
+    service1.username = "use"
+    service1.password = "pas"
+    assert service1.service_name == "ser"
+    assert service1.username == "use"
+    assert service1.password == "pas"
+    print(" OK")
+
+    print("Writing to file and retrieving :", end="")
+    service1 = Service()
+    service1.service_name = "ser"
+    service1.username = "use"
+    service1.password = "pas"
+    service1.write("test")
+    service2 = Service()
+    service2.read("test")
+    assert service1.service_name == service2.service_name
+    assert service1.username == service2.username
+    assert service1.password == service2.password
+    os.remove(os.getcwd()+"/test")
+    print(" OK")

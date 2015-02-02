e00946c8bae71abec8be918ef92ae48c4a4222fc
Jeremy Rabasco <jeremy.rabasco@epfl.ch>
2015-02-02 17:10:45 +0100

    Adds a main file and adds the possibility to exclude script from tests

---
+++ passer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3.4
+__author__ = 'Jeremy Rabasco'
+
+import service
+
+if __name__ == "__main__":
+    print("Here is PyPasser, wait for more in the future !")
--- run_tests.py
+++ run_tests.py
@@ -3,14 +3,27 @@
 __author__ = 'Jeremy Rabasco'
 
 import os
+import sys
 
 python_executable = "python3.4"
 
 test_script_name = os.path.basename(__file__)
 
+excluded = [test_script_name]
+excluded.extend(sys.argv[1:])
+
+print("Files excluded : ", end="")
+
+for script in excluded[:-1]:
+    print(script + ", ", end="")
+
+print(excluded[-1] + ".")
+
+print("***")
 
 for (dirpath, dirnames, filenames) in os.walk(os.getcwd()):
     for file in filenames:
-        if file.endswith(".py") and file != test_script_name:
+        if file.endswith(".py") and file not in excluded:
             print("Testing", file)
             os.system(python_executable + " " + file)
+            print("***")
